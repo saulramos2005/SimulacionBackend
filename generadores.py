ea275859f4eec0dc45da003d87951bfb4658d66a
saulramos2005 <Sauldra79@gmail.com>
2026-07-13 21:33:52 -0400

    Fix: GenerarCuadradosMedios

--- generadores.py
+++ generadores.py
@@ -8,14 +8,14 @@ class MetodoGeneracion(str, Enum):
 def obtener_numeros_u(metodo: MetodoGeneracion, n: int, parametros: dict = None) -> List[float]:
     parametros = parametros or {}
     if metodo == MetodoGeneracion.congruencial:
-        return generarCM(
+        return generarCongruencial(
             seed=parametros.get("semilla", 16807),
             mult=parametros.get("mult", 48271),
             mod=parametros.get("mod", 2147483647),
             n=n
         )
     elif metodo == MetodoGeneracion.medios_cuadrados:
-        return generarMC(
+        return generarCuadradosMedios(
             seed=parametros.get("semilla", 3708),
             d=parametros.get("d", 4),
             n=n
@@ -23,7 +23,7 @@ def obtener_numeros_u(metodo: MetodoGeneracion, n: int, parametros: dict = None)
     return []
 
 
-def generarCM(seed: int = 16807, mult: int = 48271, mod: int = 2147483647, n: int = 100 ):
+def generarCongruencial(seed: int = 16807, mult: int = 48271, mod: int = 2147483647, n: int = 100 ):
     cont = 0
     resultados = []
     numero = seed
@@ -33,18 +33,24 @@ def generarCM(seed: int = 16807, mult: int = 48271, mod: int = 2147483647, n: in
         cont += 1
     return resultados
 
-def generarMC(seed: int = 3708, d: int = 4, n: int = 100 ):
+def generarCuadradosMedios(seed: int = 3708, d: int = 4, n: int = 100):
     cont = 0
     resultados = []
     numero = seed
+    
     while cont < n:
         cuadrado = str(numero**2)
-        longitud = max(len(cuadrado) + 1, 2*d) if (d % 2 == 0) else max(len(cuadrado), 2*d + 1)  
-        numero = cuadrado.zfill(longitud)
-        a = ((len(numero) - d) // 2)
+        longitud_esperada = max(len(cuadrado), 2 * d)
+        if (longitud_esperada - d) % 2 != 0:
+            longitud_esperada += 1 
+            
+        numero_str = cuadrado.zfill(longitud_esperada)
+        
+        a = (len(numero_str) - d) // 2
         b = a + d 
-        numero = int(numero[a:b])
-        resultados.append(numero/(10**d))
+        
+        numero = int(numero_str[a:b])
+        resultados.append(numero / (10**d))
         cont += 1
+        
     return resultados
-
